eff0f0e0c23776c7aa6e1dcbeb40c1634e9a54a3
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-09-24 10:35:42 +0530

    array

---
+++ js/array/array1.js
@@ -0,0 +1,58 @@
+// var users = ["parva","solanki","khyatis","meet","dhaval","rahul"]
+
+// console.log(users.length);
+
+// users.push("seenu")
+// users.push("seenu","priya","rani")
+
+// console.log(users);
+
+// users.pop()
+
+// console.log(users);
+
+
+// users.unshift("rani")
+// console.log(users);
+
+// users.shift()
+// console.log(users);
+
+// users.splice(2,0,"rani")
+// users.splice(2,1,"rani")
+// users.splice(2,1,"rani","seenu","priya")
+// users.splice(2,3,"rani")
+
+// console.log(users);
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let newArray = users.concat(arr)
+// console.log(newArray);
+
+var users = ["parva","solanki","khyatis","meet","dhaval","rahul"]
+
+
+// let x = users.includes("meet")
+// console.log(x);
+
+// let x =  users.indexOf("meet")
+// console.log(x);
+
+// let x = users.join("#")
+// let x = users.join(" ")
+// let x = users.join("")
+
+// console.log(x);
+
+
+// let x = users.reverse()
+// let x = users.sort()
+
+// console.log(x);
+
+// let x = users.slice(1)
+// let x = users.slice(1,4)
+// let x = users.slice(-1)
+// let x = users.slice(-3,-1)
+
+// console.log(x);
+
