b51c6c5a1ae2cb901962af13c2be80c488065879
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-10-06 11:01:25 +0530

    array

---
+++ js/jsonArray/array1.js
@@ -0,0 +1,72 @@
+var students = [
+    {
+        id:1,
+        name:"priya",
+        age:20,
+        email:"priya@gmail.com",
+        gender:"female",
+        address:"bangalore",
+    },
+    {
+        id:2,
+        name:"rahul",
+        age:30,
+        email:"rahul@gmail.com",
+        gender:"male",
+        address:"mumbai",
+    },
+    {
+        id:3,
+        name:"priti",
+        age:20,
+        email:"priti@gmail.com",
+        gender:"female",
+        address:"bangalore",
+    },
+    {
+        id:4,
+        name:"bhuwan",
+        age:22,
+        email:"bhuwan@gmail.com",
+        gender:"male",
+        address:"mumbai",
+    }
+]
+
+// console.log(students);
+// for(let i=0;i<students.length;i++){
+//     console.log(students[i].name);
+    
+// }
+
+
+// let ans = students.map((stud)=>{
+//     return stud.name
+// })
+// console.log(ans);
+
+// map : it will return a new array with same length 
+// filter : it will return a new array with modified data
+
+// let ans = students.filter((stu)=>{
+//     return stu.age > 22
+// })
+
+// console.log(ans);
+
+
+// let ans = students.filter((stu)=>{
+//     return stu.gender == "male"
+// })
+// console.log(ans);
+
+
+// let ans = students.filter((stu)=> stu.gender == "male")
+// console.log(ans);
+
+// students.forEach((stu)=>{
+//     if(stu.gender == "male"){
+//         console.log(stu);
+        
+//     }
+// })
---
+++ js/jsonArray/array2.js
@@ -0,0 +1,94 @@
+var departments = [
+    {
+        name:"IT",
+        employees:[
+            {
+                name:"amit",
+                age:23,
+                gender:"male",
+                isActive:true
+            },
+            {
+                name:"sumit",
+                age:25,
+                gender:"male",
+                isActive:true
+            },
+            {
+                name:"seeta",
+                age:26,
+                gender:"female",
+                isActive:true
+            }
+        ]
+    },
+    {
+        name:"HR",
+        employees:[
+            
+            {
+                name:"geeta",
+                age:32,
+                gender:"female",
+                isActive:true
+            },
+            {
+                name:"harita",
+                age:36,
+                gender:"female",
+                isActive:false
+            },
+        ]
+    },
+    {
+        name:"Finance",
+        employees:[
+            
+            {
+                name:"siya",
+                age:35,
+                gender:"female",
+                isActive:true
+            },
+            {
+                name:"axit",
+                age:37,
+                gender:"male",
+                isActive:true
+            },
+            {
+                name:"payal",
+                age:39,
+                gender:"female",
+                isActive:true
+            },
+            {
+                name:"sunit",
+                age:42,
+                gender:"male",
+                isActive:false
+            }
+            
+        ]
+    },
+]
+
+// departments.forEach((depart)=>{
+//     console.log(depart.name);
+    
+// })
+
+// let ans = departments.map((depart)=>{
+//     return depart.employees;
+// })
+
+// console.log(ans);
+
+
+let ans = departments.map((depart)=>{
+    return depart.employees.filter((emp)=>{
+        return emp.gender == "male"
+    })
+})
+
+console.log(ans);
