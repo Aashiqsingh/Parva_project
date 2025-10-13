ddfae1f1d07efc9788dd641b358e2272783fba08
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-10-13 10:55:38 +0530

    onclick

---
+++ js/dom/dom1.js
@@ -0,0 +1,21 @@
+// console.log(document.getElementById("txt").innerHTML);
+// let txt = document.getElementById("txt")
+// txt.innerHTML = "Royal technosoft p ltd.";
+// txt.style.backgroundColor = "red";
+// console.log(txt.innerHTML);
+
+
+function changeTxt(){
+    let txt = document.getElementById("txt")
+    txt.innerHTML = "Royal technosoft p ltd.";
+    txt.style.backgroundColor = "red";
+
+}
+
+
+function changeSize(){
+    let img = document.getElementById("img")
+    img.style.height = "400px";
+    img.style.width = "400px";
+    img.style.borderRadius = "50%"
+}
